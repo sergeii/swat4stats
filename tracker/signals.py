sergeii/swat4stats

commit 34482a84f17ca652cac8b5065df5730d52376726

Fix coop red team

tracker/signals.py
@@ -186,7 +186,7 @@ def save_game(sender, data, server, **kwargs):
                     )[0],
                     ip=raw_player['ip'].value,
                     # team is a mapping, insert a raw value (0 or 1)
-                    team=int(raw_player['team'].raw),
+                    team=definitions.TEAM_BLUE if game.coop_game else int(raw_player['team'].raw),
                     vip=raw_player['vip'].value,
                     admin=raw_player['admin'].value,
                     dropped=raw_player['dropped'].value,

tracker/const.py
@@ -488,6 +488,7 @@ STREAM_PATTERN = {
                     'table': {
                         '0': 'swat',
                         '1': 'suspects',
+                        '2': 'swat_red',  # CO-OP Team Red
                     },
                 },
                 '7': {

commit 546827da17fcb56221d523a3264a2e2190b4d030

Fix a bug introduced by 34482a8 by fixing a missing import

tracker/signals.py
@@ -12,7 +12,7 @@ from django.db import transaction, IntegrityError
 from django.utils import timezone
 from django.utils.encoding import force_text
 
-from . import models, utils
+from . import models, utils, definitions
 from .definitions import STATS, MODE_VIP, MODE_BS, MODE_RD, MODE_SG, MODES_COOP
 
 logger = logging.getLogger(__name__)