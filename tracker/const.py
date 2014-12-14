83110b5d0a3ffb900443dd1a4f07cd4f5bf36a13
Sergei Khoroshilov <kh.sergei@gmail.com>
2014-12-15 01:01:13 +0300

    Add a list of custom maps (credits ||ESA||RIddIk)

--- tracker/const.py
+++ tracker/const.py
@@ -204,6 +204,181 @@ STREAM_PATTERN = {
             '21': '-EXP- Sellers Street Auditorium',
             '22': '-EXP- Sisters of Mercy Hostel',
             '23': '-EXP- Stetchkov Warehouse',
+
+            # Custom maps (Thanks to ||ESA||RIddIK for all the hard work getting this list done)
+            '24': 'Untitled',  # common name for all the "Unitled" maps
+            '25': 'Fairfaxe Reloaded',  # SP-FAIRFAX-Reloaded.s4m
+            '26': 'Final Crack Down, COOP',  # FinalCrackDown_COOP.s4m
+            '27': 'ApartmentNew',  # SP-ApartmentNew.s4m
+            '28': 'Saint-Paul Asylum',  # MP-Asylum.s4m, SP-St_Paul_Asylum_v1_0.s4m
+            '29': '[c=ffff00]ESA\'s [c=1e90ff]Foodwall Edit',  # MP-ESA_FoodWall.s4m
+            '30': 'La Mina v.1.5',  # MP-LA_MINA_15.s4m
+            '31': 'Operation Apollo COOP 1.1 - FIX',  # SP-Apollo_COOP-FIX.s4m
+            '32': 'Cave Complex',  # SP-CaveComplex.s4m
+            '33': 'Predator2',  # SP-SWAT4Predator2.s4m
+            '34': '{EP}Matt´s  Power Plant TSS',  # SP-EPpower-TSS.s4m
+            '35': 'Qwik Fuel (Desrat\'s SAG)',  # MP-ConvenienceStore-smash2.s4m
+            '36': 'Black Water-TTC 1.1',  # MP-BlackWater.s4m, SP-BlackWater.s4m
+            '37': 'The Watercrip',  # The_Watercrip.s4m
+            '38': '2940 Enemy Territory MP',  # MP-2940_Enemy_Territory.s4m
+            '39': 'Newfort (Revision 24) TSS',  # SP-Newfort24-TSS.s4m
+            '40': '-EXP- Drug Lab-RMX',  # SP-DrugLab-RMX.s4m
+            '41': 'Riverside Training (Desrat\'s SAG)',  # MP-Training-smash2.s4m
+            '42': 'The Building',  # SP-TheBuilding.s4m
+            '43': 'Newfort (Revision 24)',  # SP-Newfort24.s4m
+            '44': 'Wolcott (Desrat\'s SAG)',  # MP-ArmsDeal-smash2.s4m
+            '45': 'Operation Apollo 1.1 - FIXED',  # MP-Apollo-FIX.s4m
+            '46': 'Office Space V2.0',  # SP-OfficeSpacev2.s4m, MP-OfficeSpacev2.s4m
+            '47': 'panic room',  # SP-Panic-Room.s4m
+            '48': 'mistero18-byilmassacratore',  # Mistero18.s4m
+            '49': 'The Phoenix Club',  # SP-PhoenixClub.s4m, MP-PhoenixClub.s4m
+            '50': 'The Hive (VIP)',  # MP-Hive.s4m
+            '51': 'U-273',  # MP-U273.s4m, SP-U273.s4m
+            '52': 'The Manor - 1.1 - 2013',  # Sp-TheManor.s4m
+            '53': '-EXP- Newfort (Revision 27)',  # SP-Newfort27EXP.s4m
+            '54': 'City Streets 1.0',  # MP-CityStreets.s4m
+            '55': 'LA City Hall',  # MP-City_Hall_VIP.s4m, MP-City_Hall.s4m, SP-City_Hall.s4m
+            '56': '-MODv- California Security Bank - FIXED',  # MP-Bank-FIX.s4m, SP-Bank-FIX.s4m
+            '57': 'Car\'s dealer v1.2',  # SP-CARsDEALER.s4m
+            '58': 'Mout McKenna 1.0',  # MP-MoutMckenna.s4m, SP-MoutMckenna.s4m
+            '59': 'Desert ops -Village- 1.0',  # SP-DesertOpsVillage.s4m, MP-DesertOpsVillage.s4m
+            '60': 'INTERVAL - 17 - Rmx',  # SP-INTERVAL-17-rmx.s4m
+            '61': 'Ashes and Ghosts -Night-',  # MP-Ashes_And_Ghosts_Night.s4m, SP-Ashes_And_Ghosts_Night.s4m
+            '62': 'Penthouse',  # SP-Penthouse.s4m
+            '63': 'Civil Unrest',  # MP-Civil_Unrest.s4m, SP-Civil_Unrest.s4m
+            '64': 'Storm Front',  # SP-StormFront.s4m
+            '65': 'Johnson Residence',  # SP-JohnsonResidence.s4m
+            '66': 'Operation Prison Break',  # SP-Prison.s4m, MP-Prison.s4m
+            '67': 'C-Block',  # MP-CBlock.s4m, MP-CBlock1_1.s4m
+            '68': 'The Hive 1.1',  # SP-Hive1_1.s4m
+            '69': 'BattleShips',  # BattleShips.s4m
+            '70': 'Children of Taronne (Desrat\'s SAG)',  # MP-Tenement-smash2.s4m
+            '71': 'Fast Break - Through',  # MP-FastBreak-Through.s4m
+            '72': 'A-Bomb (Desrat\'s SAG)',  # MP-ABomb-smash2.s4m
+            '73': 'Ashes and Ghosts -Day-',  # SP-Ashes_And_Ghosts_Day.s4m
+            '74': 'ESA\'s 3or1',  # MP-ESA-3or1.s4m
+            '75': 'MP-Terminal',  # MP-Terminal.s4m
+            '76': 'The Entrepot',  # SP-Entrepot.s4m
+            '77': 'E.T.E.R. Training Center',  # MP-Eter_trainingcenter.s4m
+            '78': 'Subway Station v1.0',  # MP-Sub.s4m, SP-Sub.s4m
+            '79': 'Stuck in the Woods',  # SP-StuckInTheWoods.s4m
+            '80': '-EXP- Sisters of Mercy-RMX',  # SP-SistersofMercy-RMX.s4m
+            '81': 'Research Center (Desrat\'s SAG)',  # MP-DNA-smash2.s4m
+            '82': 'Brewer County (Desrat\'s SAG)',  # MP-Courthouse-smash2.s4m
+            '83': 'Stuck in the woods',  # MP-StuckInTheWoods.s4m
+            '84': '{EP}Matt´s Drugs Deal TSS',  # SP-EPdrugsdeal-TSS.s4m
+            '85': 'Snake\'s loft',  # SP-Snake-loft.s4m
+            '86': 'NewfortBeta',  # MP-NewfortBetaV2.s4m
+            '87': 'BLUES CLUB',  # MP-BCv1.s4m
+            '88': 'Fairfax Residence (Desrat\'s SAG)',  # MP-FairfaxResidence-smash2.s4m
+            '89': 'Construction',  # SP-Construction.s4m, MP-Construction.s4m
+            '90': 'Sky Tower',  # SP-SkyTower.s4m
+            '91': 'Food Wall (Desrat\'s SAG)',  # MP-Foodwall-smash2.s4m
+            '92': 'California Security Bank',  # SP-Bank.s4m
+            '93': 'Dark Waters',  # MP-DarkWaters.s4m
+            '94': 'Operation Apollo COOP 1.1',  # SP-Apollo_COOP.s4m
+            '95': 'FAYA\'s REFUGEES v1.0',  # SP-FAYAsREFUGEES.s4m
+            '96': 'Victory Imports (Desrat\'s SAG)',  # MP-AutoGarage-smash2.s4m
+            '97': 'Residential Ops.',  # SP-ResidentialOps.s4m
+            '98': '2940 Enemy Territory',  # SP-2940_Enemy_Territory.s4m
+            '99': 'Clear - Room Service',  # MP-Clear.s4m
+            '100': 'Tantive IV',  # MP-TantiveIV.s4m
+            '101': 'Red Library (Desrat\'s SAG)',  # MP-RedLibrary-smash2.s4m
+            '102': 'Dark Scarlet Restaurant',  # SP-Dark_Scarlet.s4m
+            '103': 'LA MINA',  # MP-LA_MINA.s4m
+            '104': 'Precinct HQ 1.1',  # SP-PrecinctHQ.s4m, MP-PrecinctHQ.s4m
+            '105': 'Novatech\'s Building',  # SP-NOVATECHsBUILDING.s4m
+            '106': 'Mout McKenna Snow 1.0',  # MP-MoutMckennaSnow.s4m, SP-MoutMckennaSnow.s4m
+            '107': '(SEALMAP)Desert_Dust',  # MP-Desert_Dust.s4m, SP-Desert_Dust.s4m, MP2-Desert_Dust.s4m
+            '108': 'Mogadishu Mile 1.0',  # MP-DesertOps2.s4m
+            '109': 'ATL Convention Center',  # MP-ATLConvention.s4m
+            '110': 'Gangster_Hangout',  # MP-GangsterHangout.s4m
+            '111': '(SEALMAP)Renovate TSS',  # SP-Renovate-TSS.s4m
+            '112': 'Brentwood Reloaded',  # SP-BrentReloaded.s4m, MP-BrentReloaded.s4m
+            '113': 'Operation Apollo 1.1',  # MP-Apollo.s4m, SP-Apollo.s4m
+            '114': 'The China Hotel',  # SP-CHINA-HOTEL.s4m
+            '115': 'Mad Shopping',  # SP-MadShopping.s4m, MP-MadShopping.s4m
+            '116': '(SEALMAP)School',  # SP-School.s4m, MP-School.s4m
+            '117': 'Diamond Center (Desrat\'s SAG)',  # MP-JewelryHeist-smash2.s4m
+            '118': 'Newfort2xSus',  # SP-Newfort100Sus.s4m
+            '119': 'Ocean Avenue 112',  # MP-Amityville_Horror_VIP.s4m, SP-Amityville_Horror.s4m, MP-Amityville_Horror.s4m
+            '120': '|ustt| Enemy Territory V2',  # MP-USTT_Enemy_Territory2.s4m
+            '121': 'Project -SERO- 1.0',  # MP-ProjectSero.s4m, SP-ProjectSero.s4m
+            '122': 'C-Block Taronne is back',  # SP-CBlock.s4m
+            '123': 'Reality Simulation Logistic V1.0',  # MP-Spedition.s4m
+            '124': 'Power Plant (Desrat\'s SAG)',  # MP-PowerPlant-smash2.s4m
+            '125': '5455, Carlton Way',  # SP-Getts.s4m, MP-Getts.s4m
+            '126': 'Assault On City Hall',  # SP-CityHall.s4m, MP-CityHall.s4m
+            '127': 'Fy_Iceworld2005',  # MP_Fy_iceworld2005.s4m
+            '128': 'Art Center 1.0',  # SP-ArtCenter.s4m, MP-ArtCenter.s4m
+            '129': 'Wainwright Offices',  # SP-Wainwright_Offices.s4m, MP-Wainwright_Offices.s4m
+            '130': 'Children of Tenement-RMX',  # SP-Tenement-RMX.s4m
+            '131': 'Police Station 1.0 - 2013',  # SP-PoliceStation.s4m
+            '132': 'Hotel Carlyle 2005 v.2.0',  # SP-Carlyle2k5v2-0.s4m
+            '133': 'The Asylum',  # SP-TheAsylum.s4m
+            '134': 'Final Crack Down, Barricaded Suspects',  # FinalCrackDown_BARR.s4m
+            '135': 'New Library 1.0',  # SP-NewLibrary.s4m
+            '136': 'Star Wars',  # SP-StarWars.s4m
+            '137': '-MODv- Johnson Residence - FIXED',  # SP-JohnsonResidence-FIX.s4m
+            '138': '-MODv- Hotel Carlyle 2005 - FIXED',  # SP-Carlyle2k5-FIX.s4m
+            '139': 'Old Granite Hotel (Desrat\'s SAG)',  # MP-Hotel-smash2.s4m
+            '140': 'Section 8 Fairfax Massacre',  # MP-Massacre.s4m, SP-Massacre.s4m
+            '141': 'Club ATL',  # SP-ClubATL.s4m
+            '142': 'DELTA CENTER',  # SP-DELTA-CENTER.s4m
+            '143': 'Mittelplate Alpha 1.1',  # MP-Mittelplate_Alpha.s4m
+            '144': 'panic room Coop',  # SP-PANIC-ROOM-Coop.s4m
+            '145': 'Mittelplate Alpha 1.2',  # SP-Mittelplate_Alpha.s4m
+            '146': 'Residential Ops VIP',  # MP-ResidentialOps.s4m
+            '147': 'Nova Corp.',  # SP-Nova-Corp.s4m
+            '148': 'Flash Light Tag',  # MP-FlashLightTag.s4m
+            '149': 'Mad Butcher`s Shop',  # SP-MadButcher.s4m
+            '150': 'CREEPY HOTEL',  # SP-CREEPY-HOTEL.s4m
+            '151': 'SSF Night Rescue',  # SSFNightRescue.s4m
+            '152': 'Operation Prison Break TSS',  # SP-Prison-TSS.s4m
+            '153': 'Terminal',  # SP-Terminal.s4m
+            '154': 'Paintball Madness',  # MP-PaintballMadness.s4m
+            '155': 'Madmap',  # MP-Madmap.s4m, SP-Madmap.s4m
+            '156': '[c=ffff00]ESA\'s [c=1e90ff]Riverside Edit',  # MP-ESA_Training.s4m
+            '157': 'The Baths Of Anubis',  # SP-BATHS-Of-ANUBIS.s4m
+            '158': 'DEAD_END',  # SP-DEAD_END.s4m, MP-DEAD_END.s4m
+            '159': 'KEOWAREHOUSE',  # SP-KEOWAREHOUSE.s4m
+            '160': 'DeAdMaNs UsEd CaR LoT',  # MP-UsedCarLot.s4m, SP-UsedCarLot.s4m
+            '161': 'Ventura Hotel',  # SP-Ventura.s4m, MP-Ventura.s4m
+            '162': 'SP-UNDERGROUND',  # SP-UNDERGROUND.s4m
+            '163': 'Medical Center (Desrat\'s SAG)',  # MP-Hospital-smash2.s4m
+            '164': 'The Metropol',  # SP-Metropol.s4m
+            '165': 'MP-Le Camp',  # Mp-LeCamp.s4m
+            '166': 'SubWay',  # SP-Last-Stop.s4m
+            '167': 'The Killing House -Small-',  # MP-KillingHouseSmall.s4m
+            '168': 'Reaction: Ak/Colt',  # MP-Reaction_ak_colt.s4m
+            '169': '=HT=Operation Freedom',  # MP-opfreedom.s4m
+            '170': 'Genovese & Feinbloom',  # MP-Genovese&Feinbloom.s4m, SP-Genovese&Feinbloom.s4m
+            '171': '|ustt| Enemy Territory V2 CoOp',  # SP-USTT_Enemy_Territory2.s4m
+            '172': 'The Building v1.1',  # SP-TheBuilding-v1_1.s4m
+            '173': 'AssasinationRoom, Aupicia:Clan A-T',  # MP-AssasinationRoom.s4m
+            '174': 'Department Of Criminal Justice V1',  # MP-DOCJT.s4m, SP-DOCJT.s4m
+            '175': 'Combat Zone',  # MP-CombatZone.s4m, SP-CombatZone.s4m
+            '176': 'Ventura Hotel v1r1',  # MP-ESA-Venturav1r1.s4m
+            '177': '{EP}Matt´s Medical Center TSS',  # SP-EPhosp-TSS.s4m
+            '178': 'SSF TrainingCenter 1.0',  # MP-Trainingcenter.s4m
+            '179': 'Operation Dusk Till Dawn',  # SP-Dusk.s4m
+            '180': 'MP Rush',  # MP-Rush.s4m
+            '181': 'SP-TRANSPORT',  # SP-TRANSPORT.s4m
+            '182': 'Parking Garage v1.0',  # MP-ParkingGarage.s4m
+            '183': 'Club -[*ATL*]-',  # MP-ClubATL.s4m
+            '184': 'TERRORISTA',  # SP-Terrorista.s4m
+            '185': 'Meat Barn (Desrat\'s SAG)',  # MP-MeatBarn-smash2.s4m
+            '186': 'Gris Import Export',  # SP-Import.s4m
+            '187': 'The Cinepark Conflict',  # SP-CineparkConflict.s4m
+            '188': 'Cabin Fever',  # SP-CabinFever.s4m
+            '189': 'Swanky Mansion',  # SP-SwankyMansion.s4m
+            '190': 'Apartment 525',  # SP-Apartment525.s4m
+            '191': 'Warehouse 5th Street',  # MP-Warehouse5thStreet.s4m, SP-Warehouse5thStreet.s4m
+            '192': 'Operation DayBreak',  # MP-DayBreak.s4m, SP-DayBreak.s4m
+            '193': 'Brewer County Courthouse COOP',  # SP-Courthouse.s4m
+            '194': 'Food Wall Restaurant butchered',  # MP-Foodwall2.s4m
+            '195': 'Fede\'s Nightmare',  # MP-FedesNightmare.s4m
+            '196': '(SEALMAP)Central_Base',  # MP-Central_Base.s4m, SP-Central_Base.s4m
         },
     },
     # Indicate whether the server is password protected
