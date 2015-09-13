81a20ee4f2ab4e30f2caf77ff0cae446d3efb2ba
Sergei Khoroshilov <kh.sergei@gmail.com>
2015-09-13 03:22:51 +0300

    install raven

--- swat4tracker/settings/production.py
+++ swat4tracker/settings/production.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
+import logging
 
 from .common import *
 
@@ -18,6 +19,12 @@ DATABASES = {
     },
 }
 
+INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
+
+MIDDLEWARE_CLASSES = (
+    'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
+) + MIDDLEWARE_CLASSES
+
 STATIC_ROOT = Path('/var/www/static/swat4tracker/')
 MEDIA_ROOT = Path('/var/www/media/swat4tracker/')
 
@@ -26,9 +33,60 @@ CACHES['default'] = {
     'LOCATION': 'unix:/var/run/redis/redis.sock:1',
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'syslog': {
+            'format': 'swat4tracker.%(name)s: [%(levelname)s] %(asctime)s - %(filename)s:%(lineno)s - %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'WARNING',
+            'class': 'raven.contrib.django.handlers.SentryHandler',
+        },
+        'syslog': {
+            'level': 'DEBUG',
+            'formatter': 'syslog',
+            'class': 'logging.handlers.SysLogHandler',
+            'address': '/dev/log'
+        },
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['syslog', 'sentry'],
+        },
+        'django.db.backends': {
+            'level': 'WARNING',
+            'handlers': ['syslog', 'sentry'],
+            'propagate': False,
+        },
+        'django.request': {
+            'propagate': True,
+        },
+        'django.security': {
+            'propagate': True,
+        },
+        'py.warnings': {
+            'propagate': True,
+        },
+        'django': {
+            'propagate': True,
+        },
+    },
+}
+
 CACHEOPS_REDIS = {
     'unix_socket_path': '/var/run/redis/redis.sock',
     'db': 2,
 }
 
 COMPRESS_OFFLINE = True
+
+if os.environ.get('DJ_SENTRY_DSN'):
+    RAVEN_CONFIG = {
+        'dsn': os.environ['DJ_SENTRY_DSN'],
+        'CELERY_LOGLEVEL': logging.WARNING,
+    }
--- swat4tracker/wsgi.py
+++ swat4tracker/wsgi.py
@@ -1,14 +1,8 @@
-"""
-WSGI config for swat4tracker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swat4tracker.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swat4tracker.settings')
+
+application = Sentry(get_wsgi_application())
