67a49fb59b8bbd138cf5efc95f1c65662270d6e3
Sergei Khoroshilov <kh.sergei@gmail.com>
2022-09-22 01:18:16 +0300

    add cacheback to INSTALLED_APPS

--- settings/common.py
+++ settings/common.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django_countries',
+    'cacheback',
     'raven.contrib.django.raven_compat',
     'tracker',
 )
