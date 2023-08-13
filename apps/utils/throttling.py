1a3279abf905d835713eecaa5bcdfcf07cb3ea8b
Sergei Khoroshilov <kh.sergei@gmail.com>
2023-08-13 04:26:25 +0300

    ci: fix tests

--- apps/utils/throttling.py
+++ apps/utils/throttling.py
@@ -1,12 +1,16 @@
+from typing import TYPE_CHECKING
+
 from rest_framework.request import Request
 from rest_framework.throttling import ScopedRateThrottle
-from rest_framework.views import APIView
+
+if TYPE_CHECKING:
+    from rest_framework.views import APIView
 
 
 class MethodScopedRateThrottle(ScopedRateThrottle):
     scope_methods_attr = "throttle_scope_methods"
 
-    def allow_request(self, request: Request, view: APIView) -> bool:
+    def allow_request(self, request: Request, view: "APIView") -> bool:
         self.scope_methods = getattr(view, self.scope_methods_attr, None)
 
         if self.scope_methods and request.method.upper() not in self.scope_methods:
