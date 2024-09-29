437c7f106cf5d7ed5fc834967cda01dcb2a8f4b7
Sergei K. <kh.sergei@gmail.com>
2024-09-30 01:04:16 +0300

    feat: remove redundant /data-popular-mapnames/ api endpoint (#55)

--- apps/api/views/__init__.py
+++ apps/api/views/__init__.py
@@ -1,6 +1,5 @@
 # ruff: noqa: F401
 from .articles import ArticleViewSet
 from .games import GameViewSet
-from .maps import PopularMapsViewSet
 from .search import SearchPlayersView, SearchServersView
 from .servers import PopularServersViewSet, ServerLeaderboardViewSet, ServerViewSet
